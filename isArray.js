2546aff4f6e9e74a226c7468b564513dc2d713db
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-07-15 17:48:17 +0900

    feat : Array.isArray() method

---
+++ isArray.js
@@ -0,0 +1,11 @@
+// Array.isArray()
+
+let user = {
+  name: "Mike",
+  age: 30,
+};
+
+let userList = ["Mike", "Tom", "Jane"];
+
+console.log(Array.isArray(user));
+console.log(Array.isArray(userList));
