d4ac5cefbfffe768c85d6c694b38cb1019135748
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-07-15 17:36:55 +0900

    feat : join , split method

---
+++ join.js
@@ -0,0 +1,13 @@
+// join
+let arr = ["안녕", "나는", "철수야"];
+
+let result = arr.join("  ");
+
+console.log(result);
+
+// split
+const users = "Mike,Jane,Tom,Tony";
+
+const result2 = users.split(",");
+
+console.log(result2);
